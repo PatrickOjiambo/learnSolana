fc23ae58de12e7e5488db8be825fe4f334944a99
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-11-21 01:09:19 +0300

    feat: print and println

---
+++ displaying-messages-java/2-disp.java
@@ -0,0 +1,20 @@
+//String  - group of characters(text)
+//put in double quotes
+
+//calling println()
+//Displays its parameters to the console - similar to print in python
+System.out.println("Hello");
+// "ln" is to break to a new line
+
+//calling print()
+//Doesn't print the new line
+
+//What is system.out?
+// - out is an object of the printstream class
+// - out has the print and println() methods
+// use .opeartor to access the method
+// - out refers to the standartd outpu device screen
+//System is a class(pascal case).
+//out is inside System(field)
+//use . to access out od System.
+System.out.print() //explained above
