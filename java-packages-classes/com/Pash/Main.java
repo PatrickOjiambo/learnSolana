06733c8c1ea3bee2c36b30bca1a479be440351e8
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-11-21 02:04:25 +0300

    feat: packages and classes

---
+++ java-packages-classes/com/Pash/Main.java
@@ -0,0 +1,6 @@
+package com.Pash;
+public class Main {
+    public static void main(String[] args){
+        Pash.sayHi();
+    }
+}
---
+++ java-packages-classes/com/Pash/Test.java
@@ -0,0 +1,9 @@
+public class Test{
+    public static void main(String[] args) {
+        System.out.println("Prints the string to system out");
+        sayHi();
+    }
+    public static void sayHi() {
+        System.out.println("Say hi to the user");
+    }
+}
