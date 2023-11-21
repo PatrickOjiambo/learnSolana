3063031b9b5046236e9d37917fbdac1fd641f3bf
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-11-21 20:33:33 +0300

    feat: Java overview and basics

---
+++ javabasics/Main.java
@@ -0,0 +1,17 @@
+package javabasics;
+
+public class Main {
+    //vaiable - store data - has type
+    /**
+     * Data types - string, bool, double, user defined
+     * Operators - 
+     *             arithmetic - + * - 
+     * Logical - and, or
+     * relational - <, >
+     * Conditions - if, else
+     * Loops- for, while, do while, for each
+     * Methods - divide code into smaller pieces - function in a class. - use meaningful names
+     * 
+     * 
+     */
+}
