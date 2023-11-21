24e532075f749ca84edf1a3dc3c8fb91c4a53cd2
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-11-21 19:35:04 +0300

    feat: return types

---
+++ voidReturntype/Main.java
@@ -0,0 +1,10 @@
+package voidReturntype;
+
+public class Main {
+    //the type of data returned by a method
+    //Can be anything void in this case, String, int, double
+    //void return type - return nothing
+    public static void main(String[] args) {
+        
+    }
+}
