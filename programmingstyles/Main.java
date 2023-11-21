bb32e2cbdcdb6d97855ce1080b4878fa7730e1c3
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-11-21 19:52:25 +0300

    feat: Coding style-organization

---
+++ programmingstyles/Main.java
@@ -0,0 +1,9 @@
+package programmingstyles;
+
+public class Main {
+    //Code organization
+    //Use tabs of 4 spaces.
+    public static void main(String[] args) {
+        System.out.println("Some random things");
+    }
+}
