ab114c17d9525b0e7cfb1a93d5212b98e470a777
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-11-21 19:49:08 +0300

    feat: command line args

---
+++ commandlineargs/Main.java
@@ -0,0 +1,11 @@
+package commandlineargs;
+public class Main {
+    //Data given to the main method
+    //String[] args - abbr. for arguments
+    public static void main(String[] args) {
+        //Print the first arg
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+    }
+}
