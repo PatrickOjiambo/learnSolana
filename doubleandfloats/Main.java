6f783a4f138b7ea64ea063edbccbffcea0e4e3a0
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-11-22 14:59:15 +0300

    feat: floats and doubles

---
+++ doubleandfloats/Main.java
@@ -0,0 +1,11 @@
+package doubleandfloats;
+
+public class Main {
+    /**
+     * double - number that has a decimal part, takes 8 bytes
+     * float - floating type numbers,takes 4 bytes
+     * 
+     */
+    double number = 0.9;
+    float number_ = 0.9F;
+}
