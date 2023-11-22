8da689f33bd4bca477e6f18ef990aa1b9706db0e
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-11-22 14:13:59 +0300

    feat: Constants in java

---
+++ constantsinJava/Main.java
@@ -0,0 +1,16 @@
+public class Main{
+
+    /**
+     * constants - A variable whose value cannot be changed once it has been assigned.
+     * Writted in uppercase in java - use the final keyword
+     * 
+     */
+
+
+     public static void main(String[] args) {
+        //Constants
+        final String NAME = "Pash";
+        System.out.println(NAME);
+        
+    }
+}
