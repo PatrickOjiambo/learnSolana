aca85245b5c20510fb565c24546c0bc9595af06a
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-11-22 14:20:27 +0300

    feat: identifiers in java

---
+++ identifiresinjava/Main.java
@@ -0,0 +1,9 @@
+package identifiresinjava;
+public class Main {
+    /**
+     * Identifiers are name that identify the elements in a program - names of classes, methods and variables
+     * Naming rules - letters, digits, underscore, dollar_sign - cannot be a reserved word
+     * Java is case sensitive.
+     * - use clear, descriptive names - avoid abbreviations, don't use the dollar sign while naming identifiers
+     */
+}
