b969b136f17402afac83fe0473bfdfee83f2ac96
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-11-22 15:07:28 +0300

    feat: characters

---
+++ chardatatypes/Main.java
@@ -0,0 +1,10 @@
+package chardatatypes;
+
+public class Main {
+    /**
+     * java uses unicode encoding scheme. characters are encoded to their unicode equivalents
+     */
+    char c='e';
+    char c3=68;
+    char c1='\u0041';
+}
