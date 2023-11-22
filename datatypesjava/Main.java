500a71ec7f577cd7eaee80af80c474df6ae79cf7
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-11-22 14:24:48 +0300

    feat: data types in java

---
+++ datatypesjava/Main.java
@@ -0,0 +1,10 @@
+package datatypesjava;
+
+public class Main {
+    /**
+     * Integer, Real numbers, characters, Strings, booleans(true, false)
+     * characters use single quotes.
+     * Strings use double quotes.
+     * User defined typpes - using classes an objects.
+     */
+}
