85c2ad85d07c1250353e78d4c1b7ca8bf8777fa7
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-11-22 14:52:45 +0300

    feat: bytes and values

---
+++ bytesandvalues/Main.java
@@ -0,0 +1,17 @@
+package bytesandvalues;
+
+public class Main {
+    /**
+     * bits and memory
+     * bit - smallest unit, A byte is 8 bits, mmory divided into bytes
+     * int is equal to 4 bytes
+     * byte - 1B, 8bits
+     * short -2bytes, 16bits
+     * int- 4bytes, 32bits
+     * long - 8bytes, 64bits
+     */
+    public static void main(String[] args) {
+        //finding range 
+        System.out.println(Integer.MAX_VALUE);
+    }
+}
