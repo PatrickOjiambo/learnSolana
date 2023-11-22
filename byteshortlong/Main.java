df7a77b0a1dfe1666906d50a63a713dfd9d95a66
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-11-22 14:40:34 +0300

    feat: short, long, byte

---
+++ byteshortlong/Main.java
@@ -0,0 +1,14 @@
+package byteshortlong;
+
+public class Main {
+    /**
+     * byte - A type used with integers [-128, 127]
+     * short - type used with integers [-32768, 32767]
+     * long - used with integers -letter 'l' or 'L' to tell the number is a long and not an int.
+     * type conversion = byte=>short=>int=>long
+     */
+    byte b1 = 127;
+    short s1=  5000;
+    long l1 = -7341287258725872L;
+    int i1 = 646446464;
+}
