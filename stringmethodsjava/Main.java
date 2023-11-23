aa33865b4504489f4c497370c60cb3a066b1bab8
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-11-23 21:09:28 +0300

    feat: String methods

---
+++ stringmethodsjava/Main.java
@@ -0,0 +1,17 @@
+package stringmethodsjava;
+
+public class Main {
+    /**
+     * Some String methods
+     */
+    public static void main(String[] args) {
+        String s1 = "Neso academy";
+        System.out.println(s1.toUpperCase());
+        System.out.println(s1.length());
+        System.out.println(s1.isEmpty());
+        System.out.println(s1.charAt(2));
+        System.out.println(s1.indexOf('a'));
+        System.out.println(s1.lastIndexOf("ss"));
+        System.out.println(s1.concat("Ydkjb"));
+    }
+}
