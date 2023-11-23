5df6d6a30d9540a8da1762214cd960967891d736
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-11-23 22:12:00 +0300

    feat: inc and dec operators

--- operators/Main.java
+++ operators/Main.java
@@ -4,6 +4,9 @@ public class Main {
     /**
      * assignment operator - =, assign value/expression to a variable,
      * variable = value;
+     * inc and dec operators.
+     * ++i increments the value of i by 1 and uses the new value in the statment.
+     * i++ increments the value of i by 1 and uses the original value in the statement
      */
     public static void main(String[] args) {
         int x = 3+1;
@@ -12,7 +15,13 @@ public class Main {
         int i1 = 1;
         i1 = i1 +1;
         i1 +=1;
-        
+        //inrement operator ++
+        ++i1;
+        //decrement opeartor --
+        --i1;
+        //Can be further subdived into pot and pre.
+
+
 
     }
 }
