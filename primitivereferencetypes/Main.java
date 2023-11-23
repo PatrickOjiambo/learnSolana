3f80d747034673ec8e8851527d6caa42ed1c2c0a
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-11-23 21:25:36 +0300

    feat: primitive and reference types

---
+++ primitivereferencetypes/Main.java
@@ -0,0 +1,16 @@
+package primitivereferencetypes;
+
+public class Main {
+    /**
+     * primitive types - types that hold simple values, eg int etc.
+     * reference types - types that hold complex values(objects).
+     * The value references the value
+     * s - 1004 - 800
+     * 'hello' - 1004
+     * 
+     */
+    public static void main(String[] args) {
+        //reference types - the variable contains the reference of the value
+        String s = "Hello";
+    }
+}
