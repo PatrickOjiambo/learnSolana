887edc3576a175492f098e649b541dc8944eed0a
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-11-23 20:57:39 +0300

    feat: String class

---
+++ stringdatatype/Main.java
@@ -0,0 +1,20 @@
+package stringdatatype;
+
+public class Main {
+    /**
+     * String is a class containing static methods. Return type string
+     * String objects - variables of time string, objects have methods(not static)
+     * String str = "abc";
+        is equivalent to:
+
+        char data[] = {'a', 'b', 'c'};
+        String str = new String(data)
+     * 
+     */
+    public static void main(String[] args) {
+        //Calling methods in java
+        String text = "This is an object";
+        text.toUpperCase();
+        
+    }
+}
