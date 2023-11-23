290732f6a812abc5a102bed4bb89a2013f5d4678
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-11-23 21:32:48 +0300

    feat: instanciate a string object

---
+++ instatnsiatestringobject/Main.java
@@ -0,0 +1,12 @@
+package instatnsiatestringobject;
+
+public class Main {
+    /**
+     * instantiating an object - create an object of a class
+     * String("Pash"); - this is the constructor
+     */
+    public static void main(String[] args) {
+        //Creating an object
+        String s1 = new String("Pash");
+    }
+}
